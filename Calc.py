e61206e1b48eacc8408f4e82dbe7d8ab1a9fb959
Omar Abd-Al-Fattah <omarspps5@gmail.com>
2017-09-04 09:17:40 +0200

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,7 @@
+d = input("Enter the degree of your equation : ")
+x = input("Enter the value of x : ")
+function_value = int(0) 
+  while d>= 0 : 
+    processing = x**d 
+    function_value = function_value + processing d = d-1
+print (function_value)
